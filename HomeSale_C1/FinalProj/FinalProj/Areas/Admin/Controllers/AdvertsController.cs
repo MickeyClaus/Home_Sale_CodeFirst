af788d198825d38c8bd1bfba8fbb49ee2067149e
Elnur Shahhuseynov <32703947+MickeyClaus@users.noreply.github.com>
2019-05-03 22:06:16 +0400

    All files Uploaded

---
+++ HomeSale_C1/FinalProj/FinalProj/Areas/Admin/Controllers/AdvertsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalProj.Models;
+using FinalProj.Data;
+
+namespace FinalProj.Areas.Admin.Controllers
+{[isAdmin]
+    public class AdvertsController : Controller
+    {
+        readonly FinalProjContext _context;
+        public AdvertsController()
+        {
+            _context = new FinalProjContext();
+        }
+        // GET: Admin/Adverts
+        public ActionResult Index()
+        {
+            return View(_context.Adverts.OrderBy(m=>m.CreatedAt).ToList());
+        }
+        public ActionResult OnlyVip()
+        {
+            return View(_context.Adverts.Where(m=>m.IsVip ==true).OrderBy(m => m.CreatedAt).ToList());
+        }
+        public ActionResult OnlyNew()
+        {
+            return View(_context.Adverts.Where(m => m.RentTypeId == 1).OrderBy(m => m.CreatedAt).ToList());
+        }
+        public ActionResult OnlyRent()
+        {
+            return View(_context.Adverts.Where(m => m.RentTypeId == 2).OrderBy(m => m.CreatedAt).ToList());
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
